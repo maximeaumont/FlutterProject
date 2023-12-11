924c4f0530c1f9f8a6fa3afde4110a15a830bd2c
Arthur François <arthur.francois2504@gmail.com>
2023-12-11 17:18:13 +0100

    création modle et affichage de données bruts

--- lib/ui/screens/search_sport_venue.dart
+++ lib/ui/screens/search_sport_venue.dart
@@ -1,26 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application/models/sport_venue.dart';
 
 class SearchSportVenue extends StatelessWidget {
   const SearchSportVenue({super.key});
 
+  final List<SportVenue> _sportVenues = const [
+    SportVenue(0, 'Gymnase Albert José', 'Basket', 280,
+        'gymnase_albert_jose@mail.fr', 'Nord Est Angers'),
+    SportVenue(1, 'Terrain athlétisme Lac de Maine', 'Athlétisme', 420,
+        'athletisme_ldm@mail.fr', 'A côté du Lac de Maine'),
+    SportVenue(2, 'Terrain de boules Gare du Nord', 'Pétanque', 80,
+        'petanque_gn@mail.fr', 'Derrière la gare')
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Lieux sportifs à Angers"),
-      ),
-      body: Container(
-        color: Color.fromARGB(255, 173, 14, 41), // Couleur de fond de l'écran
-        child: const Center(
-          child: Text(
-            'Bonjour Flutter !',
-            style: TextStyle(
-              fontSize: 24.0,
-              color: Colors.white,
-            ),
-          ),
+        appBar: AppBar(
+          title: const Text("Lieux sportifs à Angers"),
         ),
-      ),
-    );
+        body: Container(
+            color:
+                Color.fromARGB(255, 173, 14, 41), // Couleur de fond de l'écran
+            child: Center(
+                child: ListView.separated(
+                    itemCount: _sportVenues.length, // à remplacer,
+                    itemBuilder: (context, index) {
+                      final sportVenue = _sportVenues[index];
+                      return ListTile(
+                        leading: const Icon(Icons.place),
+                        title: Text(sportVenue.name),
+                        subtitle: Text(
+                            'Localisation : ${sportVenue.geoPosition}, Nombre de places : ${sportVenue.numberOfPlaces}'),
+                      );
+                    },
+                    separatorBuilder: (context, index) => const Divider()))));
   }
 }
