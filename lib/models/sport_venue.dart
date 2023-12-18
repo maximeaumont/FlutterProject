55dadb93561e91c9262dd4fbec70ed028a1fc8e3
Maxime AUMONT <maxime.aumont@reseau.eseo.fr>
2023-12-18 15:45:09 +0100

    AddToFavorite Functionnality

--- lib/models/sport_venue.dart
+++ lib/models/sport_venue.dart
@@ -5,7 +5,32 @@ class SportVenue {
   final int numberOfPlaces;
   final String mail;
   final String geoPosition;
+  bool isFavorite;
 
-  const SportVenue(this.id, this.name, this.activity, this.numberOfPlaces,
-      this.mail, this.geoPosition);
+  SportVenue(this.id, this.name, this.activity, this.numberOfPlaces,
+      this.mail, this.geoPosition, {this.isFavorite = false});
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'activity': activity,
+      'numberOfPlaces': numberOfPlaces,
+      'mail': mail,
+      'geoPosition': geoPosition,
+      'isFavorite': isFavorite,
+    };
+  }
+
+  factory SportVenue.fromJson(Map<String, dynamic> json) {
+    return SportVenue(
+      json['id'],
+      json['name'],
+      json['activity'],
+      json['numberOfPlaces'],
+      json['mail'],
+      json['geoPosition'],
+      isFavorite: json['isFavorite'],
+    );
+  }
 }
--- lib/ui/screens/my_favorites.dart
+++ lib/ui/screens/my_favorites.dart
@@ -1,7 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application/models/sport_venue.dart';
+import 'package:flutter_application/repository/favorites_repository.dart';
 
-class MyFavorites extends StatelessWidget {
+class MyFavorites extends StatefulWidget {
   const MyFavorites({super.key});
+
+  @override
+  _MyFavoritesState createState() => _MyFavoritesState();
+}
+
+class _MyFavoritesState extends State<MyFavorites> {
+  final FavoritesRepository _favoritesRepository = FavoritesRepository();
+  List<SportVenue> favorites = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadFavorites();
+  }
+
+  Future<void> _loadFavorites() async {
+    final loadedFavorites = await _favoritesRepository.loadFavorites();
+    setState(() {
+      favorites = loadedFavorites;
+    });
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -10,15 +34,18 @@ class MyFavorites extends StatelessWidget {
         title: const Text("Mes favoris"),
       ),
       body: Container(
-        color: Color.fromARGB(255, 255, 191, 72), // Couleur de fond de l'écran
-        child: const Center(
-          child: Text(
-            'Bonjour Flutter !',
-            style: TextStyle(
-              fontSize: 24.0,
-              color: Colors.white,
-            ),
-          ),
+        color: const Color.fromARGB(255, 255, 191, 72),
+        child: ListView.builder(
+          itemCount: favorites.length,
+          itemBuilder: (context, index) {
+            final sportVenue = favorites[index];
+            return sportVenue.isFavorite
+                ? ListTile(
+                    title: Text(sportVenue.name),
+                    subtitle: Text(sportVenue.geoPosition),
+                  )
+                : Container(); // Ne rien afficher si ce n'est pas un favori
+          },
         ),
       ),
     );
