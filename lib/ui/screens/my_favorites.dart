b462a532abcf9d2169393dfe3ec900a2a1b20dbc
Maxime AUMONT <maxime.aumont@reseau.eseo.fr>
2023-12-18 17:02:09 +0100

    Improve UI home & favorite page

--- lib/ui/screens/my_favorites.dart
+++ lib/ui/screens/my_favorites.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application/models/sport_venue.dart';
 import 'package:flutter_application/repository/favorites_repository.dart';
-import 'package:flutter_application/views/sport_venue_map_view.dart'; // Assurez-vous que le chemin est correct
+import 'package:flutter_application/views/sport_venue_map_view.dart';
 
 class MyFavorites extends StatefulWidget {
   const MyFavorites({super.key});
@@ -13,7 +13,7 @@ class MyFavorites extends StatefulWidget {
 class _MyFavoritesState extends State<MyFavorites> {
   final FavoritesRepository _favoritesRepository = FavoritesRepository();
   List<SportVenue> favorites = [];
-  int _currentIndex = 0; 
+  int _currentIndex = 0;
 
   @override
   void initState() {
@@ -33,11 +33,14 @@ class _MyFavoritesState extends State<MyFavorites> {
     return Scaffold(
       appBar: AppBar(
         title: Text(_currentIndex == 0 ? "Mes favoris" : "Carte des favoris"),
+        backgroundColor: Colors.deepPurple,
       ),
       body: _currentIndex == 0 ? _buildListView() : SportVenueMapView(favoriteVenues: favorites),
       bottomNavigationBar: BottomNavigationBar(
         onTap: onTabTapped,
         currentIndex: _currentIndex,
+        selectedItemColor: Colors.deepPurple,
+        unselectedItemColor: Colors.grey,
         items: const [
           BottomNavigationBarItem(
             icon: Icon(Icons.list),
@@ -52,23 +55,38 @@ class _MyFavoritesState extends State<MyFavorites> {
     );
   }
 
-  Widget _buildListView() {
-    return Container(
-      color: const Color.fromARGB(255, 255, 191, 72),
-      child: ListView.builder(
-        itemCount: favorites.length,
-        itemBuilder: (context, index) {
-          final sportVenue = favorites[index];
-          return sportVenue.isFavorite
-              ? ListTile(
-                  title: Text(sportVenue.name),
-                  subtitle: Text(sportVenue.geoPosition),
-                )
-              : Container(); 
-        },
-      ),
-    );
-  }
+Widget _buildListView() {
+  return ListView.builder(
+    itemCount: favorites.length,
+    itemBuilder: (context, index) {
+      final sportVenue = favorites[index];
+      return Card(
+        elevation: 4,
+        margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+        child: ListTile(
+          leading: CircleAvatar(
+            backgroundColor: Colors.deepPurple,
+            child: Text(sportVenue.name[0]), 
+          ),
+          title: Text(sportVenue.name, style: const TextStyle(fontWeight: FontWeight.bold)),
+          subtitle: Text(sportVenue.geoPosition),
+          trailing: IconButton(
+            icon: const Icon(Icons.delete, color: Colors.red),
+            onPressed: () => _removeFromFavorites(sportVenue),
+          ),
+        ),
+      );
+    },
+  );
+}
+
+void _removeFromFavorites(SportVenue venue) async {
+  setState(() {
+    favorites.remove(venue);
+    venue.isFavorite = false;
+  });
+  await _favoritesRepository.saveFavorites(favorites);
+}
 
   void onTabTapped(int index) {
     setState(() {
--- lib/views/sport_venue_map_view.dart
+++ lib/views/sport_venue_map_view.dart
@@ -11,18 +11,18 @@ class SportVenueMapView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    List<Marker> markers = favoriteVenues
-        .map((venue) => Marker(
-              width: 80.0,
-              height: 80.0,
-              point: venue.coordinates,
-              builder: (ctx) => const Icon(Icons.location_on, color: Colors.red),
-            ))
-        .toList();
+    List<Marker> markers = favoriteVenues.map((venue) {
+      return Marker(
+        width: 200.0,
+        height: 100.0,
+        point: venue.coordinates,
+        builder: (ctx) => _buildCustomMarker(venue),
+      );
+    }).toList();
 
     return FlutterMap(
       options: MapOptions(
-        center: LatLng(47.459839, -0.588140), 
+        center: LatLng(47.459839, -0.588140),
         zoom: 10.0,
       ),
       children: [
@@ -30,10 +30,22 @@ class SportVenueMapView extends StatelessWidget {
           urlTemplate: 'https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png',
           subdomains: const ['a', 'b', 'c'],
         ),
-        MarkerLayer(
-          markers: markers,
-        ),
+        MarkerLayer(markers: markers),
       ],
+    );
+  }
+
+  Widget _buildCustomMarker(SportVenue venue) {
+    return Container(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          const Expanded(
+            child: Icon(Icons.location_on, color: Colors.red, size: 40),
+          ),
+          Text(venue.name, style: const TextStyle(fontSize: 12, fontWeight: FontWeight.bold)),
+        ],
+      ),
     );
   }
 }
